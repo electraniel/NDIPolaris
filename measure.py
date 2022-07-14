60ceb5615950c0a9f0f5124446e32b6fdfb194f8
Daniel Colin <daniel.colinhernandez@gmail.com>
2022-07-14 14:16:44 +0200

    added measuring of plate tracker

--- measure.py
+++ measure.py
@@ -11,34 +11,57 @@ import pickle
 
 
 p = ndi.Polaris()
-ndi.get_plate_roms()
-p.setup()
+plate_roms = ndi.get_plate_roms()
+#in setup insert path to rom in []
+p.setup(['/ceph/mri.meduniwien.ac.at/departments/physics/fmrilab/home/dcolin/roms/P937.rom'])
 p.prepare_tracking()
 
-try: 
+#Gibt Eulerwinkel und Tranlations unterschied an. 
+#mit strg + c wird es abgebrochen und die letzte gespeicherte Position (wingel + Translation wird zurückgegeben)
 
-    while True:
-        track_data = p.perform_tracking( duration = 3., record = lambda *_: True)
-        frame = 50
-        x = np.array(track_data[frame]['stray_markers']['markers']) # waehlt die Marker aus (aus welchen Frame?)
-        x = x[:,2:] # schaut das nur x,y,z da sind
-        pickle_name = ('Test5.pkl')
-        
-        with open(pickle_name, 'wb') as handle:
-            pickle.dump(track_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        
-        M = rot.get_rot_matrix(x)
-        X_hom = np.hstack((x, np.ones((x.shape[0], 1))))
-        X = M @ X_hom.T
-        
-        B = rot.load_reference() #ladet die Referenz Platte
-        print(rot.rot2eul(M[:3,:3])) # Gibt Eulerwinkel Unterschied an 
-        #rot.plot_comp(B.T,X[:3],"Vergleich") 
+def position_plate(): 
+    try: 
+    
+        while True:
+            track_data = p.perform_tracking( duration = 3., record = lambda *_: True)
+            frame = 50
+            x = np.array(track_data[frame]['stray_markers']['markers']) # waehlt die Marker aus (aus welchen Frame?)
+            x = x[:,2:] # schaut das nur x,y,z da sind
+            pickle_name = ('Test6.pkl')
+            
+            with open(pickle_name, 'wb') as handle:
+                pickle.dump(track_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            
+            M = rot.get_rot_matrix(x)
+            X_hom = np.hstack((x, np.ones((x.shape[0], 1))))
+            X = M @ X_hom.T
+            
+            B = rot.load_reference() #ladet die Referenz Platte
+            rotation = rot.rot2eul(M[:3,:3])
+            translation = M[:3,3]
+            print('rotation: ',rotation) # Gibt Eulerwinkel Unterschied an 
+            print('Translation',translation)
+    
+    except KeyboardInterrupt:
+        return rotation, translation
 
-except KeyboardInterrupt:
+def start_plate_measurement():
+    p.setup()
+    
     pass
 
+final_position = position_plate()
+plate_roms = ndi.get_plate_roms()
+rom_start, rom_end = 0 , 18
+romlist = [plate_roms[rom_start:rom_end]]
+p.setup(*romlist)
+p.prepare_tracking()
+track_data = p.perform_tracking( duration = 10., record = lambda *_: True)
+pickle_name = 'Test{},{}.pkl'.format(rom_start,rom_end)
+with open(pickle_name, 'wb') as handle:
+    pickle.dump(track_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
+    
 """
     M = rot.get_rot_matrix(x = rot.load_measurement('/ceph/mri.meduniwien.ac.at/departments/physics/fmrilab/home/dcolin/Measuring_Scripts/Plate_test.pkl') )
 
